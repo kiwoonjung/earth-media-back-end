626efe8141cc927a3aa654e6f286e66812458a7e
kiwoonjung <kiwoon0627@gmail.com>
2024-12-26 01:03:54 -0800

    20241226 01:03 fixed errors and added mock data

--- controllers/posts.js
+++ controllers/posts.js
@@ -37,7 +37,7 @@ export const getFeedPosts = async (req, res) => {
   }
 };
 
-export const getUserPost = async (req, res) => {
+export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
     const post = await Post.find({ userId });
--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -57,6 +60,10 @@ async function startServer() {
     console.log("Connected to MongoDB");
 
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   } catch (error) {
     console.error(`Connection error: ${error.message}`);
   }
