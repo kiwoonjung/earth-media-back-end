8c140f807fda4697e3dbbee229767bc60f6ddd40
kiwoonjung <kiwoon0627@gmail.com>
2024-12-26 00:51:12 -0800

    20241226 00:50 created posts

--- controllers/users.js
+++ controllers/users.js
@@ -30,6 +30,7 @@ export const getUserFriends = async (res, req) => {
   }
 };
 
+/* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
